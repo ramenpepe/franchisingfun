b3a574e16714341bb7a64a57fd08390b52b986e9
ramenpepe <kwangwei@sol59.com>
2024-10-04 21:23:10 +0800

    update
    ubu

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
+
 import LeftNavBar from "./components/LeftNavBar";
 import Loader from "./components/Loader"
 import RightPanel from "./components/RightPanel";
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import {theme, GlobalCss} from './theme-mn';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import Flowchart from "./components/Flowchart";
-import Assets from "./components/Assets";
-import ReactFlow, { ReactFlowProvider } from '@xyflow/react';
 
-import FunctionEditor from './components/functions/FunctionEditor';
-import functions from './init/functions.json';
+
 function App() {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(0);
   const [activeItem, setActiveItem] = useState('Dashboard');
   const [curract, setCurract] = useState('...');
   const [isLoading, setIsLoading] = useState(false);
@@ -25,7 +19,6 @@ function App() {
   const [isNavVisible, setIsNavVisible] = useState(true);
   const [intervalId, setIntervalId] = useState(null);
 
-  const [functionOptions, setFunctionOptions] = useState(functions);
   //const apiUrl = process.env.REACT_APP_API_URL;
   const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
  
@@ -33,125 +26,6 @@ function App() {
   const handleNavItemClick = (item) => {
     setActiveItem(item);
 
-  };
-  const initialize = () =>{}
-  const handleLogout = (status) => {
-    // Begin the logout process
-    setIsLoading(true);
-    setIsLoggedIn(false);
-    setCurract("Logging out...");
-    console.log('1logout');
-    // Perform the logout using fetch
-    fetch(`${apiUrl}/session/logout`, {
-      method: 'GET', // Specify the request method
-      headers: {
-        'Content-Type': 'application/json', // Specify JSON content type
-        'Authorization': `Bearer ${localStorage.getItem('login')}` // Use localStorage.getItem for safer access
-      }
-    })
-      .then(response => {
-
-        if (!response.ok) {
-          // If the response is not ok, you might want to handle this differently
-          throw new Error('Network response was not ok');
-        }
-        // Logout was successful
-        if (localStorage.getItem('login')) {
-          localStorage.removeItem('login'); // Correctly use localStorage.removeItem
-        }
-        setIsLoading(false);
-        setCurract("...");
-      })
-      .catch(error => {
-
-        console.log('elogout');
-        // Handle any errors during logout
-        console.error('Error:', error);
-        // Optionally, set loading to false and update status even in case of error
-        setIsLoading(false);
-        setCurract("...");
-      });
-  };
-  const verifySession = async () => {
-
-    if (localStorage.login) {
-      // Check session expiry
-      fetch(`${apiUrl}/session/init`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${localStorage.login}`
-        }
-      })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Session validation failed');
-          }
-          return response.json(); // Assuming the server responds with JSON
-        })
-        .then(data => {
-          console.log(data); setIsLoggedIn(true);
-          return true;
-          // If success, load profile data
-
-
-
-
-        })
-        .catch(error => {
-          console.error('Error:', error);
-          setIsLoggedIn(false);
-        });
-    } else {
-      setIsLoggedIn(false);
-    }
-  }
-  const handleLogin = (auth, status) => {
-    // Begin loading process
-    setIsLoading(true);
-    console.log(apiUrl, auth);
-
-    // Perform the login using fetch
-    fetch(`${apiUrl}/session/login`, {
-      method: 'POST', // Specify the request method
-      headers: {
-        'accept': 'application/json',
-        'Content-Type': 'application/json' // Specify JSON content type
-      },
-      body: JSON.stringify(auth) // Convert the `auth` object to a JSON string
-    })
-      .then(response => {
-        console.log(response);
-        if (!response.ok) {
-          // If the response is not ok, throw an error
-          throw new Error('Network response was not ok');
-        }
-        return response.json(); // Parse the JSON in the response
-      })
-      .then(data => {
-        // Handle the successful response here
-        if (data.status == 'success') {
-          console.log('Response:', data);
-          setCurract("Logging in...");
-          localStorage.setItem('login', data.info.AuthStr); // Store the login token
-          initialize();
-        }
-        // Set a timeout to simulate loading
-        setTimeout(() => {
-          setIsLoading(false);
-          setCurract("...");
-          setIsLoggedIn(true); // Update the login status
-        }, 1500);
-      })
-      .catch(error => {
-        // Handle any errors that occur during the fetch
-        console.error('Error:', error);
-        setCurract("Credential not recognized");
-        setTimeout(() => {
-          setIsLoading(false);
-          setCurract("...");
-        }, 1500);
-      });
   };
 
   const retrieve = async (action,body,cstatus) => {
@@ -197,10 +71,8 @@ function App() {
   }, [intervalId]);
 
   return (
-    <ReactFlowProvider>
     <ThemeProvider theme={theme}>
        <CssBaseline />   <GlobalCss />
-      <GoogleOAuthProvider clientId={googleClientId}>
         <div className="App">
           {isLoading &&
             <div class={`load-bg ${loadComplete ? '' : 'complete'}`}>
@@ -211,40 +83,31 @@ function App() {
           }
 
 
-
-          {isLoggedIn ? (<div><LeftNavBar activeItem={activeItem} isNavVisible={isNavVisible} setIsNavVisible={setIsNavVisible} handleNavItemClick={handleNavItemClick} />
+<div><LeftNavBar activeItem={activeItem} isNavVisible={isNavVisible} 
+setIsNavVisible={setIsNavVisible} handleNavItemClick={handleNavItemClick} />
             <div className={`${panelIsVisible ? 'reduced' : 'fullview'} ${isNavVisible ? 'shownav' : 'hidenav'}`} >
-              {activeItem === 'Build' && <div>
-                <div>    <Flowchart/></div>
-              </div>}
+           
               {activeItem === 'Assets' && <div>
-                <div>    <Assets/></div>
+                <div>    </div>
               </div>}
               {activeItem === 'Contracts' && <div>
-                <div>
-                <FunctionEditor functionOptions={functionOptions} setFunctionOptions={setFunctionOptions} />
-        
-                </div>
+   
+               
               </div>}
 
             </div>
-            <RightPanel  panelIsVisible={panelIsVisible} setPanelIsVisible={setPanelIsVisible} handleLogout={handleLogout} />
+            <RightPanel  panelIsVisible={panelIsVisible} setPanelIsVisible={setPanelIsVisible} />
           </div>
-          ) : ("")
-          }
+           
           <Router>
             <Routes>
               <Route path="/explainer" element={<div><h1>Explainer...</h1></div>} />
-              {isLoggedIn ? (""
-              ) : (
-                <Route path="/" element={<Login onLogin={handleLogin} />} />
-              )}
+          
             </Routes>
           </Router>
-        </div></GoogleOAuthProvider>
+        </div>
         </ThemeProvider>
        
-       </ReactFlowProvider>
   );
 }
 
